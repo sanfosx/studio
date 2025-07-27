76ea9c19bcfc78a3c015a8bd71c135c0cddff7ec
Santiago Foschiatti <sanfosx@gmail.com>
2025-07-27 08:25:38 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LinkComponent hre

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,14 +1,9 @@
 
 
 import OrderPage from '@/components/order-page';
-import { ProtectedRoute } from '@/contexts/auth-provider';
 
 export default function Order() {
   return (
-    <ProtectedRoute>
-        <OrderPage />
-    </ProtectedRoute>
+    <OrderPage />
   )
 }
-
-    
